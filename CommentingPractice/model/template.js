443263011e77af07fa7637ed0749091060e2d267
iamamonte <iamamonte@gmail.com>
2020-07-02 20:30:38 -0400

    Update template.js
    Updating assignment instructions.

--- CommentingPractice/model/template.js
+++ CommentingPractice/model/template.js
@@ -12,20 +12,28 @@ export default class Template {
     {
         console.log(`
         
-        In the near future, we'll have an assignment to add documenting comments to file that handles a lot of business logic for the TurnUp app.
+       
+        In the near future, we'll have an assignment to add documenting comments to a file that handles a lot of business logic for the TurnUp app.
+                
+        As a lead up to that assignment, we're going to practice code commenting and collaboration.  The main goal of the exercise is to ensure that everyone codes and comments in the same way.  However, there are number of items that can be practiced along the way:
+                * Debugging javascript code in VS via Node.
+                * Familiarizing oneself with how a project that uses node is structured 
+                * Generally practicing javascript
+                * Utilizing hotkeys in VS Code
         
-        The purpose of this exercise is to practice commenting code and ensure that everyone comments in the same way. 
-        However, because of this project is set up, there are number of items that can be practiced:
-        * Debugging javascript code in VS via Node.
-        * Familiarizing oneself with how a project that uses node is structured 
-        * Generally practicing javascript
-        * Utilizing hotkeys in VS Code
-
+        First you'll need to ensure you're working against the latest changes. Fetch the changes from upstream. Then merge those changes into your local development branch. Lastly, push the changes resulting from that merge up to your fork.
+                
+                The task for everyone is as follows:
+                1. Add a javascript file(s) to appropriate folder(s) (services or model) 
+                2. Using someone else's code within the project, add something to the comment-practice.js. Preferably within the self-invoking function.
+                3. Make sure the code you add is documented appropriately.
+                        -> Please follow the commenting syntax for JSDocs: https://jsdoc.app/about-getting-started.html
+                4. As per the previous assignment, we'll be going through the same submission process involving branching and submitting pull requests.
         
-        The task for everyone is to add some javascript code to this file (or to another file that is then imported into this one). 
-            Try to make your code unique and make sure it is documented
-                -> Please follow the commenting syntax for JSDocs: https://jsdoc.app/about-getting-started.html
-            Within your changes make use of someone else's code.
+                I'll try to quickly accept pull requests so that people continue to have fresh material to source from for #2.
+        
+        Feel free to DM with questions/comments/concerns.
+        
             
       
 
