eb2464a641bc5466ccdff7dd91254b40bcb97b90
Cameron Senor <36214898+Cesenor@users.noreply.github.com>
2020-07-03 14:59:07 -0500

    Product js file
    File that contains a function that returns the product of two given numbers.

---
+++ CommentingPractice/services/product.js
@@ -0,0 +1,17 @@
+**
+* @file a product function that returns the products of two numbers.
+* @author Cameron Senor
+*/
+
+/**
+* Returns the product of two numbers.
+* @param {int} num1 - First number
+* @param {int} num2 - Second number
+* return int 
+*/
+function product(num1, num2){
+  return num1 * num2
+  }
+  
+  
+  export { product }
