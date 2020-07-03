23a20047621a46bdd8b0653f353d5ae5b98a77bc
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-07-03 14:19:25 -0500

    commented on the myName() method in practice.js

--- CommentingPractice/services/practice.js
+++ CommentingPractice/services/practice.js
@@ -1,6 +1,10 @@
 /** @author Sophia Sangervasi
+
 */
 
+/**
+ * prints out "Hi, my name is Sophia Sangervasi"
+ */
 function myName(){
     var Sophia = "Hi, my name is Sophia Sangervasi";
     console.log(Sophia);
