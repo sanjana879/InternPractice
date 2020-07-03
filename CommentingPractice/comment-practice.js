ec7381d860443deb90c43fca8aa01f13e573eaad
sonias02 <sonias02@cs.washington.edu>
2020-07-03 14:33:56 -0700

    added js file, edited comment-practice

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -1,11 +1,13 @@
 /**
  * @file Main
- * 
+ *
  */
 import Template from './model/template.js';
 import * as amonte from './services/capitalizer.js';
 import { myName }  from './services/practice.js';
 import * as martin from './services/spaces.js';
+import { toPigLatin } from './services/piglatin.js';
+
 
 
 import * as luis from './services/backwardsName.js';
@@ -16,9 +18,9 @@ import * as luis from './services/backwardsName.js';
     Template.printInstructions();
     var name = "AmonteAndrews";
      console.log(amonte.randomlyCapitalize(name));
-        
+
      myName();
-   
+
      var space = "space is large";
      console.log(amonte.randomlyCapitalize(space));
 
@@ -26,7 +28,10 @@ import * as luis from './services/backwardsName.js';
      console.log(luis.backwardsName(luisMyName));
 
 
- })()
+    // everyone's has been done, so I repeated one and did my own
+    console.log(amonte.randomlyCapitalize("hello"));
+    console.log(toPigLatin("book"));
+ })();
 
 
 
