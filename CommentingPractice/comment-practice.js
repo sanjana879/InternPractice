a6c1187a54c8f20fc8636d8b54c281280552d413
Luis Moran <luismoran2010@gmail.com>
2020-07-02 22:09:56 -0400

    added a js file to services folder and added to comment-practice as well as added to main.js file

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -5,12 +5,16 @@
 import Template from './model/template.js';
 import * as amonte from './services/capitalizer.js';
 
+import * as luis from './services/capitalizer.js';
+
 
 
 ( function() {
     Template.printInstructions();
     var name = "AmonteAndrews";
      console.log(amonte.randomlyCapitalize(name));
+     let myName = "Luis Moran"
+     console.log(luis.backwardsName(myName));
 
  })()
 
