9c3a13f9ed4fd498576867979a11187dcfd11aea
Mitchell Lin <ml887@cornell.edu>
2020-07-06 16:53:47 -0400

    mitchell's changes

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -8,6 +8,7 @@ import { myName }  from './services/practice.js';
 import * as martin from './services/spaces.js';
 import { toPigLatin } from './services/piglatin.js';
 import * as luis from './services/backwardsName.js';
+import * as mitchell from './services/caesarCipher.js';
 
 
 
@@ -27,6 +28,9 @@ import * as luis from './services/backwardsName.js';
 
      let greeting = "hello";
      console.log(martin.addSpace(greeting));
+
+     let message = "Mitchell";
+     console.log(mitchell.caesarCipher(message));
  })();
 
 
