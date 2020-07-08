7b4dc26117332db129d8ccb398f38dadc51d111b
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-07-08 13:45:02 -0500

    delelted abstract class

--- CommentingPractice/model/animal.js
+++ CommentingPractice/model/animal.js
@@ -68,12 +68,3 @@ class Bird extends Animal{
         heterotroph = tempHeterotroph;
     }
 }
-
-function Mammal(){
-    this.mammalName = "Mammal Name";
-    throw new Error("Instance of Abstract class cannot be instantiated");
-}
-
-Mammal.prototype.display = function(){
-    return this.mammalName;
-}
