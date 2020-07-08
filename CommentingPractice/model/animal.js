4e46835a907f0f4fe53a200e1f4c055606889d2c
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-07-08 13:01:36 -0500

    create animal.js class

---
+++ CommentingPractice/model/animal.js
@@ -0,0 +1,23 @@
+class Animal{
+
+    constructor(){
+        var name;
+        var group;
+    }
+
+    getName(){
+        return name;
+    }
+
+    setName(tempName){
+        name = tempName;
+    }
+
+    getGroup(){
+        return group;
+    }
+
+    setGroup(tempGroup){
+        group = tempGroup;
+    }
+}
