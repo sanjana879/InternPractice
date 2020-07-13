4fdd82bf556ed1bf09b5ac4d5b801dcc984e1344
iamamonte <iamamonte@gmail.com>
2020-07-13 13:41:13 -0400

    Update arithmetic.js
    Manually adjusting spacing, adding comments to the modulo function

--- CommentingPractice/services/arithmetic.js
+++ CommentingPractice/services/arithmetic.js
@@ -5,77 +5,81 @@
 
 /**
  * @description Computes the sum of two numbers
- *
- * @param {*} num1
- * @param {*} num2
- *
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * 
  * @returns sum of two numbers, or "undefined" if arguments are not numbers
  */
 function Add(num1, num2) {
-  if (!CheckTypes(num1, num2)) return;
+    if (!CheckTypes(num1, num2)) return;
 
-  return num1 + num2;
+    return num1 + num2;
 }
 
 /**
  * @description Computes the difference of two numbers
- *
- * @param {*} num1
- * @param {*} num2
- *
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * 
  * @returns difference of two numbers, or "undefined" if arguments are not numbers
  */
 function Subtract(num1, num2) {
-  if (!CheckTypes(num1, num2)) return;
+    if (!CheckTypes(num1, num2)) return;
 
-  return num1 - num2;
+    return num1 - num2;
 }
 
 /**
  * @description Computes the product of two numbers
- *
- * @param {*} num1
- * @param {*} num2
- *
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * 
  * @returns product of two numbers, or "undefined" if arguments are not numbers
  */
 function Multiply(num1, num2) {
-  if (!CheckTypes(num1, num2)) return;
+    if (!CheckTypes(num1, num2)) return;
 
-  return num1 * num2;
+    return num1 * num2;
 }
 
 /**
  * @description Computes the dividend of two numbers
- *
- * @param {*} num1
- * @param {*} num2
- *
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * 
  * @returns dividend of two numbers, or "undefined" if arguments are not numbers
  */
 function Divide(num1, num2) {
-  if (!CheckTypes(num1, num2)) return;
+    if (!CheckTypes(num1, num2)) return;
 
-  return num1 / num2;
+    return num1 / num2;
 }
 
 /**
  * @description Utility function: Verifies that both arguments are numbers
- *
- * @param {*} num1
- * @param {*} num2
- *
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * 
  * @returns whether or not both arguments are of type 'number'
  */
 function CheckTypes(num1, num2) {
-  return typeof num1 == "number" && typeof num2 == "number";
+    return (typeof num1 == 'number' && typeof num2 == 'number');
 }
+
 /**
- *
+ * @description Returns the remainder of a division operation (modulo)
+ * 
+ * @param {number} num1
+ * @param {number} num2
  */
 function Modulo(num1, num2) {
-  if (!CheckTypes(num1, num2)) return;
-  return num1 % num2;
+    if (!CheckTypes(num1, num2)) return;
+    return num1 % num2;
 }
 
 export { Add, Subtract, Multiply, Divide, Modulo };
