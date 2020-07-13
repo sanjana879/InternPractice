141d78bbf90abc3731bef6a205923e20e839bccd
iamamonte <iamamonte@gmail.com>
2020-07-13 09:41:38 -0400

    Create allModels.js
    Bringing in new models to be covered by the test coverage check.

---
+++ CommentingPractice/test/allModels.js
@@ -0,0 +1,9 @@
+import * as Animals from '../model/animal'
+import * as Languages from '../model/language'
+import * as Plants from '../model/plant'
+import * as Libraries from '../model/library'
+import * as RomanEmpire from '../model/rome'
+import * as Students from '../model/Student'
+import * as Teammates from '../model/teammate'
+
+export {Animals, Languages, Plants, Libraries, RomanEmpire, Students, Teammates}
